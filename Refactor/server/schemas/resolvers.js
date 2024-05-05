9bb0074de0701f86f5993ca151c03d8b66b347f3
Pete Oottamakorn <poottamakorn@yahoo.com>
2024-05-04 19:13:42 -0600

    completed first pass at typeDefs and resolvers

--- Refactor/server/schemas/resolvers.js
+++ Refactor/server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { signToken } = require("../../../Original/server/utils/auth");
 const { Book, User } = require("../models");
 
 const resolvers = {
@@ -7,9 +8,27 @@ const resolvers = {
     },
   },
   Mutation: {
-    login: async (parent, args) => {},
-    addUser: async (parent, args) => {
-      return User.create();
+    login: async (parent, {email, password}) => {
+      const user = await User.findOne({email});
+
+      if(!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password)
+
+      if(!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user)
+
+      return {token, user}
+    },
+    addUser: async (parent, {username, email, password}) => {
+      const user = await User.create({username, email, password});
+      const token = signToken(user);
+      return {token, user};
     },
     saveBook: async (parent, { userId, savedBook }) => {
       return User.findOneAndUpdate(
--- Refactor/server/schemas/typeDefs.js
+++ Refactor/server/schemas/typeDefs.js
@@ -2,30 +2,33 @@ const typeDefs = `
 type User {
     _id: ID
     username: String
-    email:
-    bookCount: Num
-    savedBooks: []
+    email: String
+    bookCount: Int
+    savedBooks: [Book]!
 }
 
 type Book {
-    bookId:
-    authors: [String]!
+    bookId: ID
+    authors: [Author]!
     description: String
     title: String
-    image:
-    link:
+    image: String
+    link: String
 }
 
 type Auth {
-    token:
-    user:
+    token: ID!
+    user: User
 }
 
 type Query {
-
+    me(username: String!): User
 }
 
 type Mutation {
-    
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(author: []!, description: String!, title: String!, bookId: ID!, image: String!, link: String!): User
+    removeBook(bookId: ID!): User
 }
 `;
